6eb85e90e20eba623972084b5f98ce1ba4b24157
EVE DENISON <evedenison@gmail.com>
2017-05-01 14:32:14 -0700

    fixed bolts framework bug, added map view, imported Parse framework, added button

--- LocationReminders/LocationReminders/ViewController.m
+++ LocationReminders/LocationReminders/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
+@import Parse;
+@import MapKit;
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -16,8 +22,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self requestPermissions];
+    self.mapView.showsUserLocation = YES;
+    
 }
+
+//    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+//
+//    testObject[@"testName"] = @"Eve Denison";
+//
+//    [testObject saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+//        if succeeded {
+//            NSLog(@"Success in saving test obejct.");
+//        }else {
+//            NSLog(@"There was a problem saving.  Save Error: %@",  error.localizedDescription);    
+//        }
+//
+//    }];
+//
+//    PFQuery *query = [PFQuery queryWithClassName:@"TestObject"];
+//
+//[query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable orror) {
+//    if (error) {
+//        NSLog(@"%@", error.localizedDescription);
+//    }else {
+//        NSLog(@"Query results %@", objects);
+//    }
+//
+//})];
+
+
+-(void)requestPermissions {
+    self.locationManager = [[CLLocationManager alloc]init];
+    [self.locationManager requestAlwaysAuthorization];
+
+}
+
+
+- (IBAction)location1ButtonPressed:(id)sender {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(47.6566674, -122.351096);
+    
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 500.00, 500.00);
+    
+    [self.mapView setRegion:region animated:YES];
+
+}
+
 
 
 - (void)didReceiveMemoryWarning {
