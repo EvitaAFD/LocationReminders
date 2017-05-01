145dd01ea44fd89a9a6c88b802289a7da2c429b6
EVE DENISON <evedenison@gmail.com>
2017-05-01 10:55:13 -0700

    initial commit

---
+++ LocationReminders/LocationReminders/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationReminders
+//
+//  Created by Eve Denison on 5/1/17.
+//  Copyright © 2017 Eve Denison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationReminders/LocationReminders/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationReminders
+//
+//  Created by Eve Denison on 5/1/17.
+//  Copyright © 2017 Eve Denison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
