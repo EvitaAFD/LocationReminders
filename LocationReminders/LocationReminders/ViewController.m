4c5a184db114ca04f6ad42f62e760788cd5af16f
EVE DENISON <evedenison@gmail.com>
2017-05-02 10:33:53 -0700

    added MVC structure, implemented CLLocation delegation and added Unicorn update locations features

--- LocationReminders/LocationReminders/ViewController.m
+++ LocationReminders/LocationReminders/ViewController.m
@@ -11,7 +11,7 @@
 @import Parse;
 @import MapKit;
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (strong, nonatomic) CLLocationManager *locationManager;
@@ -53,8 +53,15 @@
 
 -(void)requestPermissions {
     self.locationManager = [[CLLocationManager alloc]init];
+    
     [self.locationManager requestAlwaysAuthorization];
-
+    
+    self.locationManager.delegate = self;
+    
+//Location monitoring settings
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    self.locationManager.distanceFilter = 100; //meters
+    [self.locationManager startUpdatingLocation];
 }
 
 -(void)setMapStyle {
@@ -64,7 +71,7 @@
 
 - (IBAction)location1ButtonPressed:(id)sender {
     CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(47.6566674, -122.351096);
-    
+//Amount of map displayed on screen in meters
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 500.00, 500.00);
     
     [self.mapView setRegion:region animated:YES];
@@ -91,11 +98,13 @@
 }
 
 
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    CLLocation *location = locations.lastObject;
+    
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(location.coordinate, 200.00, 200.00);
+    
+    [self.mapView setRegion:region animated:YES];
 }
-
 
 @end
